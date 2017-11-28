10befff6e9d0277c673e7f348382d19d76ad50ce
hiroraba <xornelius0313@gmail.com>
2017-11-28 23:54:38 +0900

    Initial Commit

---
+++ CodableNSCache/CodableNSCache.h
@@ -0,0 +1,19 @@
+//
+//  CodableNSCache.h
+//  CodableNSCache
+//
+//  Created by matsuohiroki on 2017/11/28.
+//  Copyright © 2017年 matsuohiroki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodableNSCache.
+FOUNDATION_EXPORT double CodableNSCacheVersionNumber;
+
+//! Project version string for CodableNSCache.
+FOUNDATION_EXPORT const unsigned char CodableNSCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableNSCache/PublicHeader.h>
+
+
